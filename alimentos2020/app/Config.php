3f89687abe83495882350de10eaa203a759ff93d
ernestocebr91 <cebrian.paris.ernesto@iescamas.es>
2020-11-29 21:25:57 +0100

    Historico y config.php actualizado

--- alimentos2020/app/Config.php
+++ alimentos2020/app/Config.php
@@ -2,10 +2,11 @@
 
  class Config
  {
-     static public $mvc_bd_hostname = "localhost";
-     static public $mvc_bd_nombre   = "alimentos2020";
-     static public $mvc_bd_usuario  = "myjesus95";
-     static public $mvc_bd_clave    = "Q1GuQDIX";
-     static public $mvc_vis_css     = "estilo.css";
+    /* Datos facilitados por el intengrante 2.*/
+    static public $mvc_bd_hostname = "localhost";
+    static public $mvc_bd_nombre   = "alimentos52020";
+    static public $mvc_bd_usuario  = "mydanicast";
+    static public $mvc_bd_clave    = "5hA0M3yD";
+    static public $mvc_vis_css     = "estilo.css";
  }
 ?>
